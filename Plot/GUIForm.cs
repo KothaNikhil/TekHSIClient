eb761ddbc7a9c89098c1b0c22869dfa59104716a
nikhil <kotha.nikhil@tektronix.com>
2024-05-31 16:20:31 +0530

    Changes includes visa and HSI. to get data from Scope for different record lengths.

--- Plot/GUIForm.cs
+++ Plot/GUIForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,7 +27,7 @@ namespace Plot
         private readonly GraphPane graphPane;
 
         string[] rlens = new string[] {"1000","2000","5000","10000","20000","50000",
-                "100000","200000","500000","1000000","2000000","5000000","10000000","20000000","50000000"};
+                "100000","200000","500000","1000000","2000000", "5000000","10000000", "20000000", "50000000"};
         int rlenIndex = 0;
 
         static readonly Color[] Colors = {
@@ -50,15 +52,16 @@ namespace Plot
         private void TekVisaWrapper_StatusMessageUpdated(object sender, EventArgs e)
         {
             string msg = sender as string;
-            if (InvokeRequired)
-                BeginInvoke(new Action(() => { LogMsg(msg); }));
-            else
-                LogMsg(msg);
+            LogMsg(msg);
         }
 
         private void LogMsg(string msg)
         {
-            LogTextBox.AppendText(msg + "\r\n");
+            Trace.WriteLine(msg + "\r\n");
+            if (InvokeRequired)
+                BeginInvoke(new Action(() => { LogTextBox.AppendText(msg + "\r\n"); }));
+            else
+                LogTextBox.AppendText(msg + "\r\n");
         }
 
         /// <summary>
@@ -79,12 +82,15 @@ namespace Plot
         private void buttonRun_Click(object sender, EventArgs e)
         {
             if (string.Compare(ip, connected_ip, StringComparison.CurrentCultureIgnoreCase) == 0) return;
+            rlenIndex = 0;
             string[] channels = channelsTextBox.Text.Split(',');
             string visaAddress = visaAddTextBox.Text;
             hsiClient?.Dispose();
             try
             {
-                InitializeTekVisaConnections(channels, visaAddTextBox.Text);
+                bool isConnected = InitializeTekVisaConnections(channels, visaAddTextBox.Text);
+                if (!isConnected) return;
+                Thread.Sleep(1000);
                 hsiClient = HSIClient.Connect(ip, channels);
                 if (hsiClient == null) return;
                 connected_ip = ip;
@@ -98,6 +104,17 @@ namespace Plot
 
         private void HsiClient_DataAccess(HSIClient hsi, CancellationToken tok, IEnumerable<object> data, double updateTime)
         {
+            hsiClient.DataAccess -= HsiClient_DataAccess;
+
+            if(rlenIndex >= rlens.Length)
+            {
+                LogMsg("max rlen index reached");
+                Thread.Sleep(1000);
+                hsiClient?.Dispose();
+                tekVisaWrapper?.Dispose();
+                connected_ip = "-";
+                return;
+            }
             BeginInvoke(new Action(() =>
             {
                 ClearGraph();
@@ -112,12 +129,19 @@ namespace Plot
 
                     wfms.Add(wfm);
                     LogMsg(wfm.SourceName + ":" + wfm.Count);
-                    //int n = Convert.ToInt32(wfm.Count); // Replace with your desired value of n
-                    //double[] dataX = Enumerable.Range(0, n).Select(x => (double)x).ToArray();
+                    int n = Convert.ToInt32(wfm.Count); // Replace with your desired value of n
+                    double[] dataX = new double[n];// = Enumerable.Range(0, n).Select(x => (double)x).ToArray();
 
-                    //LineItem curve = graphPane.AddCurve(wfm.SourceName, dataX, wfm.ToArray(), color: Colors[0]);
+                    Parallel.ForEach(Partitioner.Create(0, n), range =>
+                    {
+                        for (var i = range.Item1; i < range.Item2; i++)
+                            dataX[i] = i;
+                    });
 
-                    //// Set the x-axis scale
+                    var dataY = wfm.ToArray();
+
+                    //LineItem curve = graphPane.AddCurve(wfm.SourceName, dataX, dataY, color: Colors[0]);
+
                     //graphPane.XAxis.Scale.Min = 0; // Minimum value
                     //graphPane.XAxis.Scale.Max = wfm.Count; // Maximum value
                 }
@@ -125,22 +149,27 @@ namespace Plot
                 //zedGraphControl1.AxisChange();
                 //zedGraphControl1.Invalidate();
             }));
+            tekVisaWrapper.SetRlen(rlens[rlenIndex++]);
+            Thread.Sleep(1000);
+            hsiClient.DataAccess += HsiClient_DataAccess;
         }
 
-        private void InitializeTekVisaConnections(string[] channels, string visaAddress)
+        private bool InitializeTekVisaConnections(string[] channels, string visaAddress)
         {
             tekVisaWrapper = new TekVisaWrapper();
             tekVisaWrapper.StatusMessageUpdated += TekVisaWrapper_StatusMessageUpdated;
             bool isConnectToVisa = tekVisaWrapper.Connect(visaAddress);
             if (!isConnectToVisa)
             {
-                //tekHSIwrapper.Dispose();
-                return;
+                LogMsg("Connection failed to visa");
+                return false;
             }
-            LogTextBox.AppendText("Connected to visa: " + isConnectToVisa + '\n');
+            LogMsg("Connected to visa: " + isConnectToVisa);
             tekVisaWrapper.TurnOnChannels(channels);
             tekVisaWrapper.SetScopeParams();
-            tekVisaWrapper.SetRlen(rlens[0]);
+            tekVisaWrapper.SetRlen(rlens[rlenIndex++]);
+            string rlen = tekVisaWrapper.Query("HOR:MODE:RECO?");
+            return true;
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
@@ -151,12 +180,15 @@ namespace Plot
         private void buttonStop_Click(object sender, EventArgs e)
         {
             hsiClient?.Dispose();
+            tekVisaWrapper?.Dispose();
+            connected_ip = "-";
         }
 
         private void ClearGraph()
         {
             // Clear the GraphPane
-            zedGraphControl1.GraphPane.CurveList.Clear();
+            graphPane.CurveList.Clear();
+            graphPane.GraphObjList.Clear();
 
             // Refresh the plot
             zedGraphControl1.AxisChange();
